d2476dc449ff565542efcda7353ff9eb322bae65
Kurosh97 <Kurosh.husseini@metropolia.fi>
2020-10-23 16:45:17 +0300

    added more code, worked on swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,16 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
    
+    
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
-        
     }
     
     
-}
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceSix"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceOne")][rightDiceNumber]
+        
+        
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber  = rightDiceNumber + 1
+    
+    
+    }
 
+}
